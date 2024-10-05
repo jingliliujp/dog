f70daf6c3b2805d1a75fb0893c70876ea59b6bd3
jingliliujp <jingliliujp@gmail.com>
2024-10-05 11:20:57 +0900

    typo fix

--- dog.go
+++ dog.go
@@ -3,5 +3,5 @@ package dog
 import "strings"
 
 func WhenGrounUp(s string) string {
-	return "When the puppy grouw up it says:" + strings.ToUpper(s)
+	return "When the puppy grows up it says:" + strings.ToUpper(s)
 }
